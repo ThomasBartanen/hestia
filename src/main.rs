c4cccc1d0980cc465a33056c29b88b648a42cfd7
Thomas Bartanen <Bartanen.Production@gmail.com>
2024-03-16 23:41:57 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
 use std::result::Result;
-use chrono::{DateTime, Utc, Local};
+use chrono::{DateTime, Local, NaiveDate, Utc};
+use database::add_tenant;
 use sqlx::{sqlite::{SqliteQueryResult, SqliteConnectOptions}, Connection, Sqlite, Executor, SqlitePool, migrate::MigrateDatabase};
+use tenant::Lease;
 
-use crate::{database::{add_expense, add_property, initialize_database}, properties::Property};
+use crate::{
+    database::{add_expense, add_property, initialize_database}, 
+    properties::Property,
+    tenant::Tenant
+};
 
 
 mod database;
 mod properties;
+mod tenant;
 
 #[derive(Debug)]
 enum ExpenseType {
@@ -53,18 +60,28 @@ impl Expense {
 #[async_std::main]
 async fn main() {
     let instances = initialize_database().await;
+    test_database(&instances).await;
+    instances.close().await;
+}
+
+async fn test_database(instances: &sqlx::Pool<Sqlite>) {
     let property = Property::new(0, "name".to_string(), "address".to_string(), "city".to_string(), "state".to_string(), "zip_code".to_string(), 10);
     match add_property(&instances, &property).await {
         Ok(_) => println!("Successfully added PROPERTY"),
         Err(e) => println!("Error when adding PROPERTY: {}", e),
     }
 
+    let lease = Lease::new(NaiveDate::from_ymd_opt(2024, 3, 1).unwrap(), NaiveDate::from_ymd_opt(2025, 2, 28).unwrap(), 1700.0, 1, "Check".to_string());
+    let tenant = Tenant::new(0,1,"John".to_string(), "Smith".to_string(), "JohnSmith@gmail.com".to_string(), "2064445555".to_string(), NaiveDate::from_ymd_opt(2024, 3, 1).unwrap());
+    match add_tenant(&instances, &tenant, &lease, 1).await {
+        Ok(_) => println!("Successfully added TENANT"),
+        Err(e) => println!("Error when adding TENANT: {}", e),
+    }
+
     let dt = Utc::now();
-    let expense = Expense::new(0, ExpenseType::Maintenance(MaintenanceType::Landscaping), 100.0, dt, "Normal Maintenance".to_string());
+    let expense = Expense::new(1, ExpenseType::Maintenance(MaintenanceType::Landscaping), 100.0, dt, "Normal Maintenance".to_string());
     match add_expense(&instances, &expense).await {
         Ok(_) => println!("Successfully added EXPENSE"),
         Err(e) => println!("Error when adding EXPENSE: {}", e),
     }
-
-    instances.close().await;
 }
