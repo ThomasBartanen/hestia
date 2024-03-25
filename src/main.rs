24a6cc57aadc48d7f7d6bf24cefbe7bbbd8c4575
Thomas Bartanen <Bartanen.Production@gmail.com>
2024-03-25 13:54:01 -0700

    Update main.rs
    Simply changing formatting, mostly because of how FeeStructure made the new() call of Lease unwieldy

--- src/main.rs
+++ src/main.rs
@@ -23,13 +23,36 @@ async fn main() {
 }
 
 async fn test_database(instances: &sqlx::Pool<Sqlite>) {
-    let property = Property::new(0, "name".to_string(), "address".to_string(), "city".to_string(), "state".to_string(), "zip_code".to_string(), 10);
+    let property = Property::new(
+        0, 
+        "name".to_string(), 
+        "address".to_string(), 
+        "city".to_string(), 
+        "state".to_string(), 
+        "zip_code".to_string(), 
+        10);
     match add_property(&instances, &property).await {
         Ok(_) => println!("Successfully added PROPERTY"),
         Err(e) => println!("Error when adding PROPERTY: {}", e),
     }
 
-    let lease = Lease::new(NaiveDate::from_ymd_opt(2024, 3, 1).unwrap(), NaiveDate::from_ymd_opt(2025, 2, 28).unwrap(), 1700.0, 1, "Check".to_string());
+    let lease = Lease::new(
+        NaiveDate::from_ymd_opt(2024, 3, 1).unwrap(),
+        NaiveDate::from_ymd_opt(2025, 2, 28).unwrap(),
+        tenant::FeeStructure::TripleNet(
+            Rent{ base_rent: 1700.0 },
+            PropertyTaxRate{ property_tax: 0.2},
+            InsuranceRate{ building_insurance: 0.15},
+            CAMRates{
+                electicity: 0.3,
+                recycling: 0.3,
+                garbage: 0.3,
+                water: 0.3,
+                landscaping: 0.3,
+                amenities: 0.3,
+                misc: 0.1,
+            }),
+        "Check".to_string());
     let tenant = Tenant::new(0,1,"John".to_string(), "Smith".to_string(), "JohnSmith@gmail.com".to_string(), "2064445555".to_string(), NaiveDate::from_ymd_opt(2024, 3, 1).unwrap());
     match add_tenant(&instances, &tenant, &lease, 1).await {
         Ok(_) => println!("Successfully added TENANT"),
