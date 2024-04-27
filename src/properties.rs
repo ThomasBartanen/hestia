4ed6ba61e00f1b5127b5bf2dab158d4d55d8075d
Thomas Bartanen <Bartanen.Production@gmail.com>
2024-04-27 13:50:08 -0700

    Update properties.rs
    Added convert_to_slint and from_row implementations

--- src/properties.rs
+++ src/properties.rs
@@ -1,3 +1,6 @@
+use crate::PropertyInput;
+use sqlx::{sqlite::SqliteRow, Row};
+
 #[derive(Debug, Clone)]
 pub struct Property {
     pub id: u16,
@@ -25,6 +28,40 @@ impl Property {
             business_insurance,
             num_units,
         }
+    }
+    pub fn convert_to_slint(&self) -> PropertyInput {
+        PropertyInput {
+            name: String::from(&self.name).into(),
+            unit_count: self.num_units.into(),
+        }
+    }
+}
+
+impl<'r> sqlx::FromRow<'r, SqliteRow> for Property {
+    fn from_row(row: &'r SqliteRow) -> Result<Self, sqlx::Error> {
+        let id: u16 = row.try_get("property_id")?;
+        let name: String = row.try_get("property_name")?;
+        let tax_string: String = row.try_get("property_tax")?;
+        let insurance_string: String = row.try_get("business_insurance")?;
+        let address_string: String = row.try_get("address")?;
+        let city_string: String = row.try_get("city")?;
+        let state_string: String = row.try_get("state")?;
+        let zip_string: String = row.try_get("zip_code")?;
+        let num_units: u16 = row.try_get("num_units")?;
+
+        let property_tax: f32 = tax_string.parse::<f32>().unwrap();
+        let business_insurance: f32 = insurance_string.parse::<f32>().unwrap();
+
+        let address: Address = Address::new(address_string, city_string, state_string, zip_string);
+
+        Ok(Property {
+            id,
+            name,
+            address,
+            property_tax,
+            business_insurance,
+            num_units,
+        })
     }
 }
 
