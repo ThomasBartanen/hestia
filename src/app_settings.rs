e8ba544784462335d67b99873f4bd1f74cd660b5
Thomas Bartanen <Bartanen.Production@gmail.com>
2024-04-26 20:22:10 -0700

    Update app_settings.rs

--- src/app_settings.rs
+++ src/app_settings.rs
@@ -1,3 +1,7 @@
+use std::path::Path;
+
+use async_std::fs;
+
 pub const TESTING_STATEMENT_PATH: &str = "E:/!Coding/Rust/hestia/statements/";
 pub const TESTING_DATABASE_PATH: &str = "E:/!Coding/Rust/hestia/";
 
@@ -12,5 +16,21 @@ impl Default for PathSettings {
             statements_path: TESTING_STATEMENT_PATH.to_owned(),
             database_path: TESTING_DATABASE_PATH.to_owned(),
         }
+    }
+}
+
+pub async fn initialize_data_paths() {
+    match Path::new(TESTING_STATEMENT_PATH).try_exists() {
+        Ok(o) => {
+            if o {
+                println!("Statement path already created")
+            } else {
+                match fs::create_dir(TESTING_STATEMENT_PATH).await {
+                    Ok(_) => println!("Successfully Created Statement Directory"),
+                    Err(e) => println!("Failed to create statement directory: {}", e),
+                }
+            }
+        }
+        Err(e) => println!("FAILED CHECKING STATEMENT PATH: {}", e),
     }
 }
