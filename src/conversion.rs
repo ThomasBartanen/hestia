815b94818a1c4f86922f1b0ff97aaa29392793a8
Thomas Bartanen <Bartanen.Production@gmail.com>
2025-03-24 16:58:05 -0700

    Create conversion.rs

---
+++ src/conversion.rs
@@ -0,0 +1,15 @@
+use crate::models::*;
+
+impl Tenant {
+    pub fn to_slint(&self) -> crate::slint_generatedApp::Tenant {
+        crate::slint_generatedApp::Tenant {
+            id: self.id,
+            name: self.name.clone().into(),
+            lease: 0,
+            property_id: 0,
+            email: self.email.clone().into(),
+            phone_number: self.phone.clone().into(),
+            move_in_date: format!("Now").into(),
+        }
+    }
+}
