ccac564329e622109115f19c6148c7f53348ebde
Thomas Bartanen <Bartanen.Production@gmail.com>
2026-02-08 10:46:53 -0800

    Update testing.rs

--- src/testing.rs
+++ src/testing.rs
@@ -183,7 +183,7 @@ pub async fn test_statements(
     }
     //println!("New Statement: {:#?}", statement);
 
-    create_statement(instances, statement, property.clone(), company, settings).await;
+    //create_statement(instances, statement, property.clone(), company, settings).await;
 
     property.business_insurance += 100.0;
     match update_property(instances, property).await {
